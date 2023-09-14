f49a7edd344f0ab628683a8d7de55048f3bc1455
Егор <124521323+f1k13@users.noreply.github.com>
2023-09-14 18:41:23 +0300

    feat: add user store, closes #10

--- src/shared/store/model/user/user.ts
+++ src/shared/store/model/user/user.ts
@@ -1,4 +1,12 @@
+import { createEvent, createStore } from 'effector';
+
 export interface IUser {
   id: number;
   username: string;
 }
+
+export const setIsAuth = createEvent<boolean>();
+
+export const $user = createStore<IUser>({ username: 'egor', id: 0 });
+
+export const $isAuth = createStore(false).on(setIsAuth, (_, value) => value);
